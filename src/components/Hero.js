43ab536799a254a9ff28f5c8c50370c308e38c3b
David Sorensen <teardownthismall@gmail.com>
2022-06-12 13:54:35 -0400

    changed hero image, styling

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,25 +1,42 @@
 import { Box, Container, Typography } from "@mui/material";
 import React from "react";
-import heroImage from "../assets/images/heroImage2.jpg";
+import heroImage from "../assets/images/KDMen.jpg";
 
 const Hero = () => {
     return (
         <Box
             sx={{
-                backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.3)),
+                backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.7)),
   url(${heroImage})`,
                 backgroundSize: "cover",
                 backgroundPosition: "50% 65%",
-                padding: "7vw 0",
             }}
         >
             <Container>
-                <Typography variant="h1" sx={{ color: "var(--fc-gray-200)" }}>
-                    2022 PFDD
-                </Typography>
-                <Typography variant="h5" sx={{ color: "var(--fc-gray-400)" }}>
-                    Learning through Listening
-                </Typography>
+                <Box
+                    sx={{
+                        padding: "14vw 0 2rem 0",
+                    }}
+                >
+                    <Typography
+                        variant="h1"
+                        sx={{
+                            color: "var(--fc-gray-200)",
+                            textShadow: "5px 5px 5px black",
+                        }}
+                    >
+                        2022 PFDD
+                    </Typography>
+                    <Typography
+                        variant="h5"
+                        sx={{
+                            color: "var(--fc-gray-400)",
+                            textShadow: "5px 5px 5px black",
+                        }}
+                    >
+                        Learning through Listening
+                    </Typography>
+                </Box>
             </Container>
         </Box>
     );
